0f3619c45c997ffc4e8d359fc85da3ea2632cc12
nayeong lee <lny021102@gmail.com>
2025-01-18 22:25:51 +0900

    네비게이션 바 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,8 @@
 // src/routes/Router.tsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+
 import App from "./App";
 import LoginPage from "./pages/loginpage/loginpage";
 import SignupPage from "./pages/signuppage/signuppage";
@@ -12,12 +14,14 @@ const AppRouter: React.FC = () => {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<App />} />
-				<Route path="/loginpage" element={<LoginPage />} />
-				<Route path="/signuppage" element={<SignupPage />} />
-				<Route path="/mainpage" element={<MainPage />} />
-				<Route path="/historypage" element={<HistoryPage />} />
-				<Route path="/questionpage" element={<QuestionPage />} />
+				<Route path="/" element={<Layout />}>
+					<Route index element={<App />} />
+					<Route path="/loginpage" element={<LoginPage />} />
+					<Route path="/signuppage" element={<SignupPage />} />
+					<Route path="/mainpage" element={<MainPage />} />
+					<Route path="/historypage" element={<HistoryPage />} />
+					<Route path="/questionpage" element={<QuestionPage />} />
+				</Route>
 			</Routes>
 		</Router>
 	);
