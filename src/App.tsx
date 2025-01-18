c5d4cebd415a3ca859fd1c660f4550d041011738
nayeong lee <lny021102@gmail.com>
2025-01-18 23:48:04 +0900

    랜딩페이지 디자인 1차 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import CustomButton from "./components/CustomButton";
 import CustomColumn from "./components/CustomColumn"
 import CustomFont from "./components/CustomFont"
-import { useNavigate } from "react-router-dom";
-import CustomRow from "./components/CustomRow";
+import StyledImg from "./components/StyledImg";
+import moon from '../src/assets/Landing_icon_moon.svg';
+import post from '../src/assets/Landing_img_post.svg';
 
 function App() {
   const navigate = useNavigate();
@@ -15,19 +17,30 @@ function App() {
   };
 
   return (
-    <CustomColumn $width="100%" $minHeight="90vh" $gap='30vh'>
+    <CustomColumn $width="100%" $minHeight="100vh" $gap="10vh" style={{ background: "linear-gradient(to bottom, #2C1B5B, #B8A4D4, #BAA8D9)" }}>
 
-      <CustomRow $width="90%" $justifycontent="flex-start">
-        <CustomFont $color='black' $font="1.5rem">서비스명</CustomFont>
-      </CustomRow>
+      <CustomColumn $width="90%" $height="30%" $justifycontent="center" $alignitems='flex-start' $gap="2rem">
+        <CustomColumn $height="5vh" />
+        <StyledImg src={moon} />
+        <CustomFont $color='white' $font="2rem">8시 우체통</CustomFont>
 
-      <CustomColumn $width="90%" $gap="1rem">
-        <CustomButton onClick={GoLogin}>
-          <CustomFont $color="black">로그인하러 가기</CustomFont>
-        </CustomButton>
-        <CustomButton onClick={GoSignup}>
-          <CustomFont $color="black">회원가입하러 가기</CustomFont>
-        </CustomButton>
+        <CustomColumn $width="100%" $alignitems="flex-start" $justifycontent="center" $gap='0.5rem'>
+          <CustomFont $color='white' $font="1rem">당신의 마음 속</CustomFont>
+          <CustomFont $color='white' $font="1rem">이야기를 전해주세요.</CustomFont>
+        </CustomColumn>
+      </CustomColumn>
+
+      <CustomColumn $width="90%" $height="70%" $alignitems="flex-end" $gap="0">
+        <StyledImg src={post} $height="30%" />
+        <CustomColumn $width="100%" $gap="1rem">
+          <CustomButton onClick={GoLogin} $backgroundColor="#694FA0" $borderRadius="2rem">
+            <CustomFont $color="white">로그인 하기</CustomFont>
+          </CustomButton>
+          <CustomButton onClick={GoSignup} $backgroundColor="#694FA0" $borderRadius="2rem">
+            <CustomFont $color="white">회원가입 하기</CustomFont>
+          </CustomButton>
+        </CustomColumn>
+        <CustomColumn $height="3vh" />
       </CustomColumn>
     </CustomColumn>
   )
