0cac85d1c02e1853346f883670ac6eeedda70cec
nayeong lee <lny021102@gmail.com>
2025-01-18 22:46:51 +0900

    히스토리 화면 템플릿 추가(디자인 x)

--- src/pages/historypage/historypage.tsx
+++ src/pages/historypage/historypage.tsx
@@ -1,13 +1,38 @@
-import CustomColumn from "../../components/CustomColumn"
-import CustomFont from "../../components/CustomFont"
+import CustomBox from "../../components/CustomBox";
+import CustomColumn from "../../components/CustomColumn";
+import CustomFont from "../../components/CustomFont";
+import CustomDivider from "../../components/CustomDivider";
 
 function HistoryPage() {
+	// 질문, 답변 mockData
+	const historyData = [
+		{ question: "가장 행복했던 순간은?", answer: "너희들이 태어났을 때" },
+		{ question: "너무 힘들었던 순간은 언제인가요?", answer: "IMF 때 우리 가족을 먹여살려야 할 때" },
+		{ question: "화가 났던 때는 언제인가요?", answer: "너희가 말을 안 들으면...^^" },
+	];
 
 	return (
-		<CustomColumn $width="100%" $minHeight="100vh">
-			<CustomFont $color='black'>여기는 히스토리 페이지</CustomFont>
+		<CustomColumn $width="100%" $minHeight="100vh" $alignitems="center" $justifycontent="flex-start">
+			<CustomColumn $height="1vh" />
+			{historyData.map((item, index) => (
+				<CustomBox
+					key={index}
+					$width="90%"
+					$height="auto"
+					$alignitems="center"
+					$justifycontent="center"
+					$padding="1rem 0.5rem"
+					$backgroundcolor="#F4F4F5"
+				>
+					<CustomColumn $width="100%" $height="auto">
+						<CustomFont $color="black">Q. {item.question}</CustomFont>
+						<CustomDivider $width="90%" $height="1px" $backgroundcolor="#D9D9D9" />
+						<CustomFont $color="black">A. {item.answer}</CustomFont>
+					</CustomColumn>
+				</CustomBox>
+			))}
 		</CustomColumn>
-	)
+	);
 }
 
-export default HistoryPage
+export default HistoryPage;
