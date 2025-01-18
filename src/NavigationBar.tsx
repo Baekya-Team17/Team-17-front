ec868680a3514ba092c54bdb82ecfc790f2efbe0
nayeong lee <lny021102@gmail.com>
2025-01-18 23:13:31 +0900

    NavBar 디자인 반영 완료

--- src/NavigationBar.tsx
+++ src/NavigationBar.tsx
@@ -6,10 +6,16 @@ import CustomButton from "./components/CustomButton";
 import CustomRow from "./components/CustomRow";
 import CustomBox from "./components/CustomBox";
 import CustomFont from "./components/CustomFont";
+import StyledImg from "./components/StyledImg";
 
-import { MdHomeFilled } from "react-icons/md";
-import { MdLocalPostOffice } from "react-icons/md";
-import { BsPersonFill } from "react-icons/bs";
+// import { MdHomeFilled } from "react-icons/md";
+// import { MdLocalPostOffice } from "react-icons/md";
+// import { BsPersonFill } from "react-icons/bs";
+
+import NavImg from './assets/Nav_background.svg';
+import NavIcon1 from './assets/Nav_icon_post.svg';
+import NavIcon2 from './assets/Nav_icon_letter.svg';
+import NavIcon3 from './assets/Nav_icon_my.svg';
 
 const NavigationBar: React.FC = () => {
 	const location = useLocation();
@@ -29,28 +35,28 @@ const NavigationBar: React.FC = () => {
 		return null;
 	}
 
-	const homeIconColor = location.pathname === "/mainpage" ? "#694FA0" : "black";
-	const postOfficeIconColor = location.pathname === "/historypage" ? "#694FA0" : "black";
-	const myPageIconColor = "black";
+	// const homeIconColor = location.pathname === "/mainpage" ? "#694FA0" : "black";
+	// const postOfficeIconColor = location.pathname === "/historypage" ? "#694FA0" : "black";
+	// const myPageIconColor = "black";
 
 	return (
-		<CustomBox $width="100%" $height="auto" $backgroundcolor="#D9D9D9" $borderradius="0" $padding="0.5rem">
+		<CustomBox $width="100%" $height="auto" $backgroundcolor="#D9D9D9" $borderradius="0" $padding="0.5rem" $backgroundimg={NavImg}>
 			<CustomRow $width="100%" $justifycontent="center" $alignitems="center" $gap="2rem">
 				<CustomButton $flexDirection="column" $backgroundColor="transparent" $width="33%" $height="auto" $padding="0"
 					$gap="0.5rem" onClick={GoMain}>
-					<MdHomeFilled size="2rem" color={homeIconColor} />
-					<CustomFont $color={homeIconColor}>홈</CustomFont>
+					<StyledImg src={NavIcon1} />
+					<CustomFont $color="black">홈</CustomFont>
 				</CustomButton>
 
 				<CustomButton $flexDirection="column" $backgroundColor="transparent" $width="33%" $height="auto" $padding="0"
 					$gap="0.5rem" onClick={GoHistory}>
-					<MdLocalPostOffice size="2rem" color={postOfficeIconColor} />
-					<CustomFont $color={postOfficeIconColor}>우체통</CustomFont>
+					<StyledImg src={NavIcon2} />
+					<CustomFont $color="black">우체통</CustomFont>
 				</CustomButton>
 
 				<CustomButton $flexDirection="column" $backgroundColor="transparent" $width="33%" $height="auto" $padding="0" $gap="0.5rem">
-					<BsPersonFill size="2rem" color={myPageIconColor} />
-					<CustomFont $color={myPageIconColor}>마이페이지</CustomFont>
+					<StyledImg src={NavIcon3} />
+					<CustomFont $color="black">마이페이지</CustomFont>
 				</CustomButton>
 			</CustomRow>
 		</CustomBox>
